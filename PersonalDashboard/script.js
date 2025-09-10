1c706c3d82fa5bae2fad2fae373dc725812dc503
Sakibur Rahman <sakiburrahmannnn@gmail.com>
2025-09-10 15:35:28 +0600

    Initial commit

--- PersonalDashboard/script.js
+++ PersonalDashboard/script.js
@@ -1,1 +1,13 @@
-console.log("JS is working!");
+// Counter variable
+let count = 0;
+
+// Grab HTML elements
+const countEl = document.getElementById('count');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const resetBtn = document.getElementById('reset');
+
+// Add click events
+incrementBtn.onclick = () => countEl.textContent = ++count;
+decrementBtn.onclick = () => countEl.textContent = --count;
+resetBtn.onclick = () => countEl.textContent = count = 0;
